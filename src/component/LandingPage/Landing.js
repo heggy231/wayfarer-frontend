28694b50e3839086236811dcf5a4afb23049eefa
Heggyhere <heggyy@gmail.com>
2019-03-04 22:48:23 -0800

    add favicon, font Awesome index.html

--- src/component/LandingPage/Landing.js
+++ src/component/LandingPage/Landing.js
@@ -5,8 +5,22 @@ class Landing extends Component{
     render(){
         return(
             <div >
-            
-
+              <div id="home">
+                <div id="landing-page-flex">
+                    <div className="top-level-wrapper-flex">
+                        <div id="top-level">
+                            <h1>Wayfarer</h1>
+                        </div>
+                        <div id="subHeader">
+                            <h2>Visit and Post your experiences</h2>
+                        </div>
+                        <div class="bouncy arrow-point-down">
+                        <a class="fas fa-angle-double-down" href="#project">
+                        </a>
+                        </div>
+                    </div>
+                </div>
+              </div> 
             </div>
         );
     }
